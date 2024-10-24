211025727e5459d0af7c7decf43c3ac1a5be6166
sandro tambutto <sandrotambutto@gmail.com>
2024-10-24 20:56:23 -0300

    subo funcion division

---
+++ calculadora.G7/division.js
@@ -0,0 +1,9 @@
+function division(num1, num2){
+    if (num2 == 0) {
+        return "Error, ingrese otro valor distinto de cero";
+    } else {
+    return num1/num2;
+}
+}
+
+Molule.export = division;
---
+++ calculadora.G7/main.js
@@ -0,0 +1,10 @@
+const sumar = require("./suma.js");
+const resta = require("./resta.js");
+const division = require("./divicion.js");
+const multiplicacion = require("./multiplicacion.js");
+
+console.log(sumar());
+console.log(resta());
+console.log(division());
+console.log(multiplicacion());
+
