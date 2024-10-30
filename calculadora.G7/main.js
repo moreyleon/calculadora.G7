2d1f8f1fb4d8041ba8447f0d2795ff107fb13342
SashaCanoN <sashi.pitu24@gmail.com>
2024-10-29 21:38:48 -0300

    Update main.js

--- calculadora.G7/main.js
+++ calculadora.G7/main.js
@@ -1,10 +1,15 @@
 const sumar = require("./suma.js");
-const resta = require("./resta.js");
-const division = require("./divicion.js");
-const multiplicacion = require("./multiplicacion.js");
+const restar = require("./resta.js");
+const division = require("./division.js");
+const multiplicar = require("./multiplicacion.js");
+const potencia = require("./potencia.js");
+const raiz = require("./raiz.js");
+const multiplicar = require("./multiplicacion.js");
 
-console.log(sumar());
-console.log(resta());
-console.log(division());
-console.log(multiplicacion());
 
+console.log(sumar(2,3));
+console.log(restar(5,2));
+console.log(division(10,2));
+console.log(multiplicar(4,3));
+console.log(potencia(5,2));
+console.log(raiz(4));
